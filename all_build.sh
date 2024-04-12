1d35afff570b14967e5a597bd4a5aedf0709896e
kao.zhu <kao.zhu@horizon.ai>
2024-04-12 10:12:28 +0800

    chore(all_build.sh): Remove COLCON_IGNORE for performance_test pkg

--- all_build.sh
+++ all_build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 touch \
-    ./src/tools/benchmark/performance_test/COLCON_IGNORE \
     ./src/tools/benchmark/performance_test_ros1_msgs/COLCON_IGNORE \
     ./src/tools/benchmark/performance_test_ros1_publisher/COLCON_IGNORE \
     ./src/tools/benchmark/performance_report/COLCON_IGNORE \
