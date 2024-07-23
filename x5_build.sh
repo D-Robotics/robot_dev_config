efc50b273449404f79e41e236d7dcf9ecad80813
kao.zhu <kao.zhu@horizon.ai>
2024-07-23 14:32:14 +0800

    feat(x5_build.sh): Enable pkg building

--- x5_build.sh
+++ x5_build.sh
@@ -6,9 +6,7 @@ touch \
     ./src/box/hobot_perception/hobot_bev/COLCON_IGNORE \
     ./src/box/hobot_perception/hobot_falldown_detection/COLCON_IGNORE \
     ./src/box/hobot_perception/elevation_net/COLCON_IGNORE \
-    ./src/box/hobot_perception/hand_lmk_detection/COLCON_IGNORE \
     ./src/box/hobot_perception/hobot_centerpoint/COLCON_IGNORE \
-    ./src/box/hobot_perception/hobot_stereonet/COLCON_IGNORE \
     ./src/box/hobot_perception/mono3d_indoor_detection/COLCON_IGNORE \
     ./src/box/hobot_perception/parking_perception/COLCON_IGNORE \
     ./src/box/hobot_tts/COLCON_IGNORE \
@@ -17,7 +15,6 @@ touch \
     ./src/box/hobot_slam/COLCON_IGNORE \
     ./src/box/hobot_llm/COLCON_IGNORE \
     ./src/box/hobot_sensor/hobot_imu_sensor/COLCON_IGNORE \
-    ./src/box/hobot_sensor/hobot_mipi_cam/COLCON_IGNORE \
     ./src/box/hobot_sensor/hobot_rgbd_cam/COLCON_IGNORE \
     ./src/box/hobot_sensor/hobot_stereo_usb_cam/COLCON_IGNORE \
     ./src/box/hobot_hdmi/COLCON_IGNORE \
