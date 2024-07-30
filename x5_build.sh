b6d67357cb7725d4db98db2e847a13d0b6f2707b
kao.zhu <kao.zhu@horizon.ai>
2024-07-30 18:50:27 +0800

    chore(x5_build.sh): Enable repos building

--- x5_build.sh
+++ x5_build.sh
@@ -4,23 +4,11 @@ touch \
     ./src/tools/benchmark/performance_test_ros1_publisher/COLCON_IGNORE \
     ./src/tools/benchmark/performance_report/COLCON_IGNORE \
     ./src/box/hobot_perception/hobot_bev/COLCON_IGNORE \
-    ./src/box/hobot_perception/hobot_falldown_detection/COLCON_IGNORE \
-    ./src/box/hobot_perception/elevation_net/COLCON_IGNORE \
     ./src/box/hobot_perception/hobot_centerpoint/COLCON_IGNORE \
-    ./src/box/hobot_perception/mono3d_indoor_detection/COLCON_IGNORE \
-    ./src/box/hobot_perception/parking_perception/COLCON_IGNORE \
     ./src/box/hobot_tts/COLCON_IGNORE \
-    ./src/box/hobot_trigger/COLCON_IGNORE \
-    ./src/box/hobot_vio/COLCON_IGNORE \
-    ./src/box/hobot_slam/COLCON_IGNORE \
-    ./src/box/hobot_llm/COLCON_IGNORE \
-    ./src/box/hobot_sensor/hobot_imu_sensor/COLCON_IGNORE \
     ./src/box/hobot_sensor/hobot_rgbd_cam/COLCON_IGNORE \
-    ./src/box/hobot_sensor/hobot_stereo_usb_cam/COLCON_IGNORE \
     ./src/box/hobot_hdmi/COLCON_IGNORE \
     ./src/box/hobot_audio/COLCON_IGNORE \
     ./src/box/hobot_interactions/audio_tracking/COLCON_IGNORE \
-    ./src/box/hobot_interactions/COLCON_IGNORE \
+    ./src/box/hobot_interactions/audio_control/COLCON_IGNORE \
     ./src/app/line_follower/COLCON_IGNORE \
-    ./src/tools/benchmark/performance_test/COLCON_IGNORE \
-
