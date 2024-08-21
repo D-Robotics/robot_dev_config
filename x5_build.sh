1abb24dc368ee1788bc3f949b7779c0151fdaed3
kao.zhu <kao.zhu@d-robotics.cc>
2024-08-21 15:36:51 +0800

    chore(scripts): Update x5 repos

--- x5_build.sh
+++ x5_build.sh
@@ -5,10 +5,11 @@ touch \
     ./src/tools/benchmark/performance_report/COLCON_IGNORE \
     ./src/box/hobot_perception/hobot_bev/COLCON_IGNORE \
     ./src/box/hobot_perception/hobot_centerpoint/COLCON_IGNORE \
+    ./src/box/hobot_perception/parking_perception/COLCON_IGNORE \
+    ./src/box/hobot_perception/hobot_stereonet/COLCON_IGNORE \
     ./src/box/hobot_tts/COLCON_IGNORE \
     ./src/box/hobot_sensor/hobot_rgbd_cam/COLCON_IGNORE \
-    ./src/box/hobot_hdmi/COLCON_IGNORE \
+    ./src/box/hobot_sensor/hobot_stereo_usb_cam/COLCON_IGNORE \
     ./src/box/hobot_audio/COLCON_IGNORE \
     ./src/box/hobot_interactions/audio_tracking/COLCON_IGNORE \
     ./src/box/hobot_interactions/audio_control/COLCON_IGNORE \
-    ./src/app/line_follower/COLCON_IGNORE \
