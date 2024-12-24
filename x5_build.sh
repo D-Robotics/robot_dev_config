f1aa20903ba7ec45aa3578f0ad5174da8dda0e40
GuangYuanNing <guangyuan.ning@horizon.ai>
2024-12-24 12:35:04 +0000

    add audio and zed cam, update version

--- x5_build.sh
+++ x5_build.sh
@@ -6,10 +6,5 @@ touch \
     ./src/box/hobot_perception/hobot_bev/COLCON_IGNORE \
     ./src/box/hobot_perception/hobot_centerpoint/COLCON_IGNORE \
     ./src/box/hobot_perception/parking_perception/COLCON_IGNORE \
-    ./src/box/hobot_llm/COLCON_IGNORE \
-    ./src/box/hobot_tts/COLCON_IGNORE \
     ./src/box/hobot_sensor/hobot_rgbd_cam/COLCON_IGNORE \
     ./src/box/hobot_sensor/hobot_stereo_usb_cam/COLCON_IGNORE \
-    ./src/box/hobot_audio/COLCON_IGNORE \
-    ./src/box/hobot_interactions/audio_tracking/COLCON_IGNORE \
-    ./src/box/hobot_interactions/audio_control/COLCON_IGNORE \
