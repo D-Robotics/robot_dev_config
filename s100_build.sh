f462e1cc47f42a0b1ceeaa7e86bba66b07e2f9ed
GuangYuanNing <guangyuan.ning@horizon.ai>
2025-03-21 06:58:53 +0000

    update s100_build.sh

--- s100_build.sh
+++ s100_build.sh
@@ -8,16 +8,17 @@ touch \
     ./src/box/hobot_perception/parking_perception/COLCON_IGNORE \
     ./src/box/hobot_sensor/hobot_rgbd_cam/COLCON_IGNORE \
     ./src/box/hobot_sensor/hobot_stereo_usb_cam/COLCON_IGNORE \
-    ./src/tools/COLCON_IGNORE \
-    ./src/box/hobot_codec/COLCON_IGNORE \
     ./src/box/hobot_llm/COLCON_IGNORE \
     ./src/box/hobot_slam/COLCON_IGNORE \
-    ./src/box/hobot_websocket/COLCON_IGNORE \
     ./src/box/hobot_perception/COLCON_IGNORE \
-    ./src/app/COLCON_IGNORE \
+    ./src/box/hobot_tts/COLCON_IGNORE \
     ./src/box/hobot_audio/COLCON_IGNORE \
     ./src/box/hobot_clip/COLCON_IGNORE \
+    ./src/app/hobot_chatbot/COLCON_IGNORE \
     ./src/box/hobot_vio/COLCON_IGNORE \
     ./src/box/hobot_hdmi/COLCON_IGNORE \
     ./src/box/hobot_dnn/dnn_benchmark_example/COLCON_IGNORE \
     ./src/box/hobot_dnn/dnn_node_sample/COLCON_IGNORE \
+    ./src/box/hobot_interactions/audio_control/COLCON_IGNORE \
+    ./src/box/hobot_interactions/audio_tracking/COLCON_IGNORE \
+
