dd196c0f21a3f3657cad3a75fbf4eff6e9857249
kao.zhu <kao.zhu@d-robotics.cc>
2025-04-29 16:52:20 +0800

    feat(s100_build.sh): Enable bev and centerpoint building

--- s100_build.sh
+++ s100_build.sh
@@ -3,8 +3,6 @@ touch \
     ./src/tools/benchmark/performance_test_ros1_msgs/COLCON_IGNORE \
     ./src/tools/benchmark/performance_test_ros1_publisher/COLCON_IGNORE \
     ./src/tools/benchmark/performance_report/COLCON_IGNORE \
-    ./src/box/hobot_perception/hobot_bev/COLCON_IGNORE \
-    ./src/box/hobot_perception/hobot_centerpoint/COLCON_IGNORE \
     ./src/box/hobot_perception/parking_perception/COLCON_IGNORE \
     ./src/box/hobot_sensor/hobot_rgbd_cam/COLCON_IGNORE \
     ./src/box/hobot_sensor/hobot_stereo_usb_cam/COLCON_IGNORE \
