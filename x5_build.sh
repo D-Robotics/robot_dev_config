c18226ecd8e777f3fa4c43c3f374662fefedc47c
zixi01.chen <zixi01.chen@horizon.ai>
2025-05-12 17:06:12 +0800

    feat(llamacpp): add rdk s100 hobot llamacpp

--- x5_build.sh
+++ x5_build.sh
@@ -8,3 +8,4 @@ touch \
     ./src/box/hobot_perception/parking_perception/COLCON_IGNORE \
     ./src/box/hobot_sensor/hobot_rgbd_cam/COLCON_IGNORE \
     ./src/box/hobot_sensor/hobot_stereo_usb_cam/COLCON_IGNORE \
+    ./src/box/hobot_llamacpp/COLCON_IGNORE \
