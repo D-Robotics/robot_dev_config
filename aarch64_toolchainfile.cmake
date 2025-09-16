b60803b094ee02d9615d923f94bcb35f778798d1
zhengzheng.ge <zhengzheng.ge@horizon.cc>
2025-09-16 18:58:13 +0800

    fix(compile): [cr_id_skip] Fix compile error

--- aarch64_toolchainfile.cmake
+++ aarch64_toolchainfile.cmake
@@ -21,6 +21,8 @@ set(Qt5Widgets_DIR ${CMAKE_CURRENT_LIST_DIR}/../../sysroot_docker/usr/lib/aarch6
 set(Qt5Gui_DIR ${CMAKE_CURRENT_LIST_DIR}/../../sysroot_docker/usr/lib/aarch64-linux-gnu/cmake/Qt5Gui)
 set(Qt5Test_DIR ${CMAKE_CURRENT_LIST_DIR}/../../sysroot_docker/usr/lib/aarch64-linux-gnu/cmake/Qt5Test)
 set(Qt5Concurrent_DIR ${CMAKE_CURRENT_LIST_DIR}/../../sysroot_docker/usr/lib/aarch64-linux-gnu/cmake/Qt5Concurrent)
+set(X11_Xrandr_LIB ${CMAKE_CURRENT_LIST_DIR}/../../sysroot_docker/usr/lib/aarch64-linux-gnu/libXrandr.so.2)
+set(X11_Xaw_LIB    ${CMAKE_CURRENT_LIST_DIR}/../../sysroot_docker/usr/lib/aarch64-linux-gnu/libXaw.so)
 set(BUILD_TESTING off)
 set(BUILD_HBMEM ON)
 set(PYTHON_SOABI cpython-310-aarch64-linux-gnu)
--- x5_build.sh
+++ x5_build.sh
@@ -21,16 +21,24 @@ LINK_DIRS=(
     include/uuid
     include/aarch64-linux-gnu/qt5
     include/opencv4
+    lib/aarch64-linux-gnu/qt5
     lib/libOpenNI.so
     lib/aarch64-linux-gnu/libpcl_common.so
     lib/aarch64-linux-gnu/libOpenNI2.so
     lib/aarch64-linux-gnu/libblas.so
     lib/aarch64-linux-gnu/liblapack.so
     lib/aarch64-linux-gnu/libyaml-cpp.so.0.7.0
+    lib/aarch64-linux-gnu/libdraco.a
+    lib/aarch64-linux-gnu/libdraco.so.4.0.0
+    lib/aarch64-linux-gnu/libz.so
+    lib/aarch64-linux-gnu/libz.so.1.2.11
+    lib/aarch64-linux-gnu/librt.a
+    lib/aarch64-linux-gnu/libtinyxml.so
+    lib/aarch64-linux-gnu/libtinyxml.so.2.6.2
 )
 
 pre_function() {
-    echo "Running pre_function"
+    echo -e "\033[1;32m[INFO]\033[0m Running pre_function"
     
     # ros部分package的cmake中硬编码头文件在/usr/include中
     SYSROOT_DIR="`pwd`/../sysroot_docker/usr"
@@ -38,7 +46,7 @@ pre_function() {
     SYMLINKS_FILE="/tmp/sysroot_temp_symlinks.txt"
     >"$SYMLINKS_FILE"
 
-    echo "Creating symlinks..."
+    echo -e "\033[1;32m[INFO]\033[0m Creating symlinks..."
     for dir in "${LINK_DIRS[@]}"; do
       src="${SYSROOT_DIR}/${dir}"
       dst="${TARGET_DIR}/${dir}"
@@ -54,11 +62,12 @@ pre_function() {
     for file in "${COLCON_IGNORE_LIST[@]}"; do
         touch "$file"
     done
+    echo -e "\033[1;32m[INFO]\033[0m End pre_function"
 }
 
 post_function() {
-    echo "Running post_function"
-    if [[ "$platform" == "X5" ]] && [[ -f "/tmp/sysroot_temp_symlinks.txt" ]]; then
+    echo -e "\033[1;32m[INFO]\033[0m Running post_function"
+    if [[ -f "/tmp/sysroot_temp_symlinks.txt" ]]; then
       while read -r path; do
         if [ -L "$path" ]; then
           rm "$path"
@@ -72,6 +81,7 @@ post_function() {
             rm "$file"
         fi
     done
+    echo -e "\033[1;32m[INFO]\033[0m End post_function"
 }
 
 main() {
