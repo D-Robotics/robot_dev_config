57d5dbcb0403a2d17eefaf02b722055911313e9c
zhengzheng.ge <zhengzheng.ge@horizon.ai>
2025-11-25 22:39:59 +0800

    feat(rtabmap): [cr_id_skip] Compile rtabmap

--- aarch64_toolchainfile.cmake
+++ aarch64_toolchainfile.cmake
@@ -22,7 +22,7 @@ set(Qt5Gui_DIR ${CMAKE_CURRENT_LIST_DIR}/../../sysroot_docker/usr/lib/aarch64-li
 set(Qt5Test_DIR ${CMAKE_CURRENT_LIST_DIR}/../../sysroot_docker/usr/lib/aarch64-linux-gnu/cmake/Qt5Test)
 set(Qt5Concurrent_DIR ${CMAKE_CURRENT_LIST_DIR}/../../sysroot_docker/usr/lib/aarch64-linux-gnu/cmake/Qt5Concurrent)
 set(X11_Xrandr_LIB ${CMAKE_CURRENT_LIST_DIR}/../../sysroot_docker/usr/lib/aarch64-linux-gnu/libXrandr.so.2)
-set(X11_Xaw_LIB    ${CMAKE_CURRENT_LIST_DIR}/../../sysroot_docker/usr/lib/aarch64-linux-gnu/libXaw.so)
+set(X11_Xaw_LIB ${CMAKE_CURRENT_LIST_DIR}/../../sysroot_docker/usr/lib/aarch64-linux-gnu/libXaw.so)
 set(BUILD_TESTING off)
 set(BUILD_HBMEM ON)
 set(PYTHON_SOABI cpython-310-aarch64-linux-gnu)
