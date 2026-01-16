8ec6cdde9acf46ca21c0471b317ffbdb9b5d6001
zhengzheng.ge <zhengzheng.ge@horizon.ai>
2026-01-16 15:50:15 +0800

    feat(rtabmap-gui): [cr_id_skip] Add qt5 component cmake for rtabmap ui

--- aarch64_toolchainfile.cmake
+++ aarch64_toolchainfile.cmake
@@ -21,6 +21,7 @@ set(Qt5Widgets_DIR ${CMAKE_CURRENT_LIST_DIR}/../../sysroot_docker/usr/lib/aarch6
 set(Qt5Gui_DIR ${CMAKE_CURRENT_LIST_DIR}/../../sysroot_docker/usr/lib/aarch64-linux-gnu/cmake/Qt5Gui)
 set(Qt5Test_DIR ${CMAKE_CURRENT_LIST_DIR}/../../sysroot_docker/usr/lib/aarch64-linux-gnu/cmake/Qt5Test)
 set(Qt5Concurrent_DIR ${CMAKE_CURRENT_LIST_DIR}/../../sysroot_docker/usr/lib/aarch64-linux-gnu/cmake/Qt5Concurrent)
+set(Qt5PrintSupport_DIR ${CMAKE_CURRENT_LIST_DIR}/../../sysroot_docker/usr/lib/aarch64-linux-gnu/cmake/Qt5PrintSupport)
 set(X11_Xrandr_LIB ${CMAKE_CURRENT_LIST_DIR}/../../sysroot_docker/usr/lib/aarch64-linux-gnu/libXrandr.so.2)
 set(X11_Xaw_LIB ${CMAKE_CURRENT_LIST_DIR}/../../sysroot_docker/usr/lib/aarch64-linux-gnu/libXaw.so)
 set(crypto_LIB ${CMAKE_CURRENT_LIST_DIR}/../../sysroot_docker/usr/lib/aarch64-linux-gnu/libcrypto.so)
