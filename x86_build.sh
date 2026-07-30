06cf4df2e022073f458a71265d013d76d4cd232d
yangyangren <yangyangren@qq.com>
2026-07-30 10:55:00 +0800

    Add speech_agent in ros2_alpha.repos(tros_1.1.0)

--- x86_build.sh
+++ x86_build.sh
@@ -31,4 +31,7 @@ touch \
     ./src/box/hobot_perception/dstereo_occnet/COLCON_IGNORE \
     ./src/box/hobot_clip/COLCON_IGNORE \
     ./src/box/hobot_llamacpp/COLCON_IGNORE \
-    ./src/box/hobot_tts/COLCON_IGNORE
+    ./src/box/hobot_tts/COLCON_IGNORE \
+    ./src/box/speech_agent_tts/COLCON_IGNORE \
+    ./src/box/speech_agent_asr/COLCON_IGNORE \
+    ./src/box/speech_agent_audio/COLCON_IGNORE \
